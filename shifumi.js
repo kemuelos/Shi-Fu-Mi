2c65fb2d42dedfccc5ef175992087cf154e6ce6f
kemuelos <syo.mimo14@gmail.com>
2022-05-03 00:45:15 -0400

    backend-complet

--- shifumi.js
+++ shifumi.js
@@ -1,26 +1,45 @@
+function random(min = 0, max = 2) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function win_condition(player_choice, computer_choice) {
+	if (player_choice === computer_choice) {
+		return "égalité";
+	} else if (
+		(player_choice === "pierre" && computer_choice === "ciseaux") ||
+		(player_choice === "ciseaux" && computer_choice === "papier") ||
+		(player_choice === "papier" && computer_choice === "pierre")
+	) {
+		return "gagné";
+	} else {
+		return "perdu";
+	}
+}
+
 // get html elements
 const play_button = document.querySelector("#play");
 const pseudo_input = document.querySelector("#pseudo");
 
+const player_choices = document.querySelectorAll(".player_choice");
+
 const retour_button = document.querySelector("#return");
+
+const player_h1 = document.querySelector("#player_move");
+const computer_h1 = document.querySelector("#computer_move");
 
 // get divs by id
 const div_menu = document.getElementById("menu");
 const div_jeu = document.getElementById("jeu");
 
-// get pseudo
-const pseudo = pseudo_input.value;
-
 play_button.addEventListener("click", (e) => {
-	// console.log(div_jeu.classList);
+	// console.log(pseudo_input.value);
 
 	// toggle : css class on/off for this selected element
 	div_jeu.classList.toggle("invisible");
 
-	// console.log(div_jeu.classList);
-
-	console.log(div_menu.classList);
 	div_menu.classList.toggle("invisible");
+
+	const pseudo = pseudo_input.value;
 });
 
 retour_button.addEventListener("click", (e) => {
@@ -28,3 +47,26 @@ retour_button.addEventListener("click", (e) => {
 
 	div_jeu.classList.toggle("invisible");
 });
+
+const IA_moves = ["pierre", "papier", "ciseaux"];
+
+// player choice
+for (const button of player_choices) {
+	button.addEventListener("click", (e) => {
+		const player_choice = e.target.id;
+
+		const computer_move = random();
+		const computer_choice = IA_moves[computer_move];
+		// console.log({ computer_move });
+
+		player_h1.innerText = player_choice;
+		computer_h1.innerText = computer_choice;
+		const resultat = win_condition(player_choice, computer_choice);
+		console.log(win_condition(player_choice, computer_choice));
+		setTimeout(() => {
+			alert(resultat);
+			
+		}, 200);
+	});
+}
+
